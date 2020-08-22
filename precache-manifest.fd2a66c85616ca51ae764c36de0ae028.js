0b274585946df77b5d46f9d60acfce58dc1cea85
sakshi-vyas <sakshivyas216@gmail.com>
2020-08-22 09:51:22 +0530

    Updates

---
+++ precache-manifest.fd2a66c85616ca51ae764c36de0ae028.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32defda155269c048246af57543fb2d8",
+    "url": "/CounterApp/index.html"
+  },
+  {
+    "revision": "8aef36e586031a9adda7",
+    "url": "/CounterApp/static/css/main.6ae3c3a7.chunk.css"
+  },
+  {
+    "revision": "acc720cb523a0960244b",
+    "url": "/CounterApp/static/js/2.662efe8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CounterApp/static/js/2.662efe8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aef36e586031a9adda7",
+    "url": "/CounterApp/static/js/main.bc3c7de8.chunk.js"
+  },
+  {
+    "revision": "420c819c315dc22fd81e",
+    "url": "/CounterApp/static/js/runtime-main.a6339423.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CounterApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
